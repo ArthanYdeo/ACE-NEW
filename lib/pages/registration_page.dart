5671348c6824b8502f80623547d4a56cbed07bd1
BalegLosi <@johnmarkmejia124@gmail.com>
2023-03-24 19:47:35 +0800

    changed the Students/$_idnum.text

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -373,7 +373,7 @@ class _RegisterPageState extends State<RegisterPage> {
                                       DatabaseReference dbReference = FirebaseDatabase
                                           .instance
                                           .ref()
-                                          .child("Students/${_fname.text}/");
+                                          .child("Students/${_idnum.text}/");
                                       await dbReference
                                           .child("fullname")
                                           .set(_fname.text);
